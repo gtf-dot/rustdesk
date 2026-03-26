ae2bc30c8c7e3cc769cab8cb67f43946d88da0d6
German Torres <gtf@grisas.co>
2026-03-26 10:05:37 -0500

    --password enabled with no install check

--- src/core_main.rs
+++ src/core_main.rs
@@ -421,7 +421,7 @@ pub fn core_main() -> Option<Vec<String>> {
                 return None;
             }
             if args.len() == 2 {
-                if crate::platform::is_installed() && is_root() {
+                if is_root() {
                     if let Err(err) = crate::ipc::set_permanent_password(args[1].to_owned()) {
                         println!("{err}");
                     } else {
